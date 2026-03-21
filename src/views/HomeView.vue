a6aeaf26b6b0a143bf4de819f4638de54e91b0d4
Patience Wangechi <patiencekiboi2@gmail.com>
2026-03-21 06:42:16 +0300

    new scroll features

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -49,23 +49,73 @@ function goToPanel(index) {
 function handleScroll(event) {
   //@wheel and the function handleScroll work majorly for the mouse thus inability to work on another device
   // I want to fix this version by enabling each scroll event on different devices to be accommodated
-  if (event.deltaY > 0) {
+
+  function goNext() {
     if (currentIndex.value < TOTAL_PANELS - 1) {
-      currentIndex.value++
+      currentIndex.value ++
     }
-  } else {
+  }
+
+  function goPrev() {
     if (currentIndex.value > 0) {
-      currentIndex.value--
+      currentIndex.value --
+    }
+  }
+
+//  ---Mouse Wheel ---
+function handleWheel(event){
+  event.preventDefault()
+  if (event.deltaY > 0){
+    goNext()
+  }
+  else{
+    goPrev()
+  }
+}
+  
+// Touch
+//Record where the finger first touched
+function handleTouchStart(event) {
+  touchStartX.value = event.touches[0].clientX
+}
+//When finger lifts, compare where it ended vs where it started
+function handleTouchEnd(event) {
+  const touchEndX = event.changedTouches[0].clientX
+  const diff = touchStartX.value - touchEndX
+
+  //diff is positive = finger moved LEFT = user wants to go forward
+  //diff is negative = RIGHT= user wants to go backward
+  //We use 50px as a minimum swipe distance to avoid accidental triggers
+  if (diff > 50) {
+    goNext()
+   
+    } else if (diff < -50){
+      goPrev()
     }
   }
 }
-
+// Keyboard
+function handleKeydown(event) {
+  //event.key tell us which key is pressed
+  if (event.key === 'ArrowRight' || event.key === 'ArrowDown') {
+    goNext()
+  }else if (event.key === 'ArrowLeft' || event.key === "ArrowUp"){
+    goPrev()
+  }
+}
 onMounted(() => {
     window.addEventListener('wheel',handleScroll, {passive: false})
+    window.addEventListener("touchstart", handleTouchStart)
+    window.addEventListener("touchend",handleTouchEnd)
+    window.addEventListener('keydown', handleKeydown)
 
 })
 onUnmounted(() => {
     window.removeEventListener("wheel", handleScroll)
+    window.removeEventListener("touchstart", handleTouchStart)
+    window.removeEventListener("touchend",handleTouchEnd)
+    window.removeEventListener('keydown', handleKeydown)
+
 })
 </script>
 
